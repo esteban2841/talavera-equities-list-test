8b8d077eebc9c314d590f18d86d82b68a93701c5
Esteban Puentes Roldan <rapuentes@uan.edu.co>
2022-05-22 14:06:00 -0500

    added collors for each species type

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,12 @@ export const API_HOST = "https://rickandmortyapi.com/api"
 
 export const CHARACTER_SPICIES_COLORS ={
     human:"#cfed9f",
-    alien:"#203b5c"
+    alien:"#203b5c",
+    cronenberg:"#a7dc53",
+    humanoid:"#e7d9cc",
+    unknown:"grey",
+    poopybutthole:"#a63d2e",
+    animal:"orange",
+    robot:"#616260",
+    disease:"green",
 }
