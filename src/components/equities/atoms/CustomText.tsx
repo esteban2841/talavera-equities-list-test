0c3997790201fae2ba7ffe075ea1db603ed36c09
Esteban Puentes <rapuentes@uan.edu.co>
2024-12-18 13:59:32 -0500

    Feature: created customText and replaced old logic also erased styles deprecated

---
+++ src/components/equities/atoms/CustomText.tsx
@@ -0,0 +1,70 @@
+import { Text, type TextProps, StyleSheet } from 'react-native';
+
+export type CustomTextProps = TextProps & {
+  type: 'default' | 'title' | 'subtitle' | 'link' | 'increaseSymbol';
+  children?: React.ReactNode;
+};
+
+export function CustomText({
+  style,
+  children,
+  type = 'default',
+  ...rest
+}: CustomTextProps) {
+
+  return (
+    <Text
+      style={[
+        type === 'default' ? styles.default : undefined,
+        type === 'title' ? styles.title : undefined,
+        type === 'subtitle' ? styles.subtitle : undefined,
+        type === 'link' ? styles.link : undefined,
+        type === 'increaseSymbol' ? styles.increaseSymbol : undefined,
+        style,
+      ]}
+      {...rest}
+    >{children}</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  default: {
+      color: "white",
+      fontSize: 16,
+      fontFamily: 'Poppins, sans-serif',
+      fontWeight: '400',
+      fontStyle: 'normal',
+      marginRight: 4,
+    },
+    title: {
+      color: 'rgb(255, 255, 255)',
+      fontSize: 18,
+      fontFamily: 'Poppins, sans-serif',
+      fontWeight: '400',
+      fontStyle: 'normal',
+      marginRight: 4,
+    },
+    increaseSymbol: {
+      color: 'rgb(0, 153, 117)',
+      fontSize: 18,
+      fontFamily: 'Poppins, sans-serif',
+      fontWeight: '400',
+      fontStyle: 'normal',
+      marginRight: 4,
+    },
+    subtitle: {
+      color: 'rgb(255, 255, 255)',
+      opacity: 0.9,
+      fontSize: 14,
+      marginRight: 10,
+      paddingTop: 20,
+      fontFamily: 'Poppins, sans-serif',
+      fontWeight: '400',
+      fontStyle: 'normal',
+    },
+    link: {
+      lineHeight: 30,
+      fontSize: 16,
+      color: '#0a7ea4',
+    },
+});
--- src/components/equities/atoms/StocksOverview.js
+++ src/components/equities/atoms/StocksOverview.js
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native-web"
 import { hotUpdate } from "../../../utils/equities"
 import { StyleSheet } from "react-native"
+import { CustomText } from "../atoms/CustomText"
 
 const StocksOverview = ({equities}) => {
 
@@ -9,19 +10,19 @@ const StocksOverview = ({equities}) => {
   return (
     <View style={styles.mainContainer} >
       <View style={styles.hotStockContainer} >
-        <Text style={styles.stockTitle} >Daily Hot Stock</Text>
+        <CustomText type="default" >Daily Hot Stock</CustomText>
         <View style={styles.spacing} >
 
           <View style={styles.stockContainerHeader} >
-            <Text style={styles.stockContainerHeaderItem}>{symbol}</Text>
-            <Text style={styles.stockIncrementSymbol}>+</Text>
-            <Text style={styles.stockContainerHeaderItem}>{daily_change}</Text>
+            <CustomText type="default">{symbol}</CustomText>
+            <CustomText type="increaseSymbol">+</CustomText>
+            <CustomText type="title">{daily_change}</CustomText>
           </View>
         </View>
         <View style={styles.spacing} >
           <View style={styles.stockContainerHeader}>
-            <Text style={styles.stockContainerFooterItem}>Text{name}</Text>
-            <Text style={styles.stockContainerFooterItem}>{price}</Text>
+            <CustomText type="subtitle">{name}</CustomText>
+            <CustomText type="subtitle">{price}</CustomText>
           </View>
 
         </View>
@@ -47,46 +48,11 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
   },
-  stockTitle:{
-    color: "white",
-    fontSize: 16,
-    fontFamily: 'Poppins, sans-serif',
-    fontWeight: '400',
-    fontStyle: 'normal',
-  },
   stockContainerHeader:{
     display: "flex",
     flexDirection: "row",
     justifyContent: "flex-start",
     alignItems: "center",
-  },
-  stockContainerHeaderItem:{
-    color: 'rgb(255, 255, 255)',
-    fontSize: 18,
-    fontWeight: "bold",
-    marginRight: 10,
-    fontFamily: 'Poppins, sans-serif',
-    fontWeight: '400',
-    fontStyle: 'normal',
-  },
-  stockIncrementSymbol:{
-    color: 'rgb(0, 153, 117)',
-    fontSize: 18,
-    fontFamily: 'Poppins, sans-serif',
-    fontWeight: '400',
-    fontStyle: 'normal',
-    fontWeight: "bold",
-    marginRight: 4,
-  },
-  stockContainerFooterItem:{
-    color: 'rgb(255, 255, 255)',
-    opacity: 0.9,
-    fontSize: 14,
-    marginRight: 10,
-    paddingTop: 20,
-    fontFamily: 'Poppins, sans-serif',
-    fontWeight: '400',
-    fontStyle: 'normal',
   },
   spacing:{
     flex: 1,
